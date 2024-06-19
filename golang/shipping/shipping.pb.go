9c654f74ff06e94d897f576c83169741aef0a7e9
iAmGreatt <greatjohncena9@gmail.com>
2024-06-19 05:58:13 +0000

    Generate protobuf code

--- golang/shipping/shipping.pb.go
+++ golang/shipping/shipping.pb.go
@@ -20,143 +20,79 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type CreateOrderRequest struct {
-	UserId               int64    `protobuf:"varint,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
-	Items                []*Item  `protobuf:"bytes,2,rep,name=items,proto3" json:"items,omitempty"`
-	TotalPrice           float32  `protobuf:"fixed32,3,opt,name=total_price,json=totalPrice,proto3" json:"total_price,omitempty"`
+type CreateShippingRequest struct {
+	Address              string   `protobuf:"bytes,1,opt,name=address,proto3" json:"address,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CreateOrderRequest) Reset()         { *m = CreateOrderRequest{} }
-func (m *CreateOrderRequest) String() string { return proto.CompactTextString(m) }
-func (*CreateOrderRequest) ProtoMessage()    {}
-func (*CreateOrderRequest) Descriptor() ([]byte, []int) {
+func (m *CreateShippingRequest) Reset()         { *m = CreateShippingRequest{} }
+func (m *CreateShippingRequest) String() string { return proto.CompactTextString(m) }
+func (*CreateShippingRequest) ProtoMessage()    {}
+func (*CreateShippingRequest) Descriptor() ([]byte, []int) {
 	return fileDescriptor_218d76102d381bfb, []int{0}
 }
 
-func (m *CreateOrderRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_CreateOrderRequest.Unmarshal(m, b)
+func (m *CreateShippingRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateShippingRequest.Unmarshal(m, b)
 }
-func (m *CreateOrderRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_CreateOrderRequest.Marshal(b, m, deterministic)
+func (m *CreateShippingRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateShippingRequest.Marshal(b, m, deterministic)
 }
-func (m *CreateOrderRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreateOrderRequest.Merge(m, src)
+func (m *CreateShippingRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateShippingRequest.Merge(m, src)
 }
-func (m *CreateOrderRequest) XXX_Size() int {
-	return xxx_messageInfo_CreateOrderRequest.Size(m)
+func (m *CreateShippingRequest) XXX_Size() int {
+	return xxx_messageInfo_CreateShippingRequest.Size(m)
 }
-func (m *CreateOrderRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_CreateOrderRequest.DiscardUnknown(m)
+func (m *CreateShippingRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateShippingRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_CreateOrderRequest proto.InternalMessageInfo
+var xxx_messageInfo_CreateShippingRequest proto.InternalMessageInfo
 
-func (m *CreateOrderRequest) GetUserId() int64 {
+func (m *CreateShippingRequest) GetAddress() string {
 	if m != nil {
-		return m.UserId
-	}
-	return 0
-}
-
-func (m *CreateOrderRequest) GetItems() []*Item {
-	if m != nil {
-		return m.Items
-	}
-	return nil
-}
-
-func (m *CreateOrderRequest) GetTotalPrice() float32 {
-	if m != nil {
-		return m.TotalPrice
-	}
-	return 0
-}
-
-type Item struct {
-	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *Item) Reset()         { *m = Item{} }
-func (m *Item) String() string { return proto.CompactTextString(m) }
-func (*Item) ProtoMessage()    {}
-func (*Item) Descriptor() ([]byte, []int) {
-	return fileDescriptor_218d76102d381bfb, []int{1}
-}
-
-func (m *Item) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Item.Unmarshal(m, b)
-}
-func (m *Item) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Item.Marshal(b, m, deterministic)
-}
-func (m *Item) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Item.Merge(m, src)
-}
-func (m *Item) XXX_Size() int {
-	return xxx_messageInfo_Item.Size(m)
-}
-func (m *Item) XXX_DiscardUnknown() {
-	xxx_messageInfo_Item.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_Item proto.InternalMessageInfo
-
-func (m *Item) GetName() string {
-	if m != nil {
-		return m.Name
+		return m.Address
 	}
 	return ""
 }
 
-type CreateOrderResponse struct {
-	OrderId              int32    `protobuf:"varint,1,opt,name=order_id,json=orderId,proto3" json:"order_id,omitempty"`
+type CreateShippingResponse struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *CreateOrderResponse) Reset()         { *m = CreateOrderResponse{} }
-func (m *CreateOrderResponse) String() string { return proto.CompactTextString(m) }
-func (*CreateOrderResponse) ProtoMessage()    {}
-func (*CreateOrderResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_218d76102d381bfb, []int{2}
+func (m *CreateShippingResponse) Reset()         { *m = CreateShippingResponse{} }
+func (m *CreateShippingResponse) String() string { return proto.CompactTextString(m) }
+func (*CreateShippingResponse) ProtoMessage()    {}
+func (*CreateShippingResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_218d76102d381bfb, []int{1}
 }
 
-func (m *CreateOrderResponse) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_CreateOrderResponse.Unmarshal(m, b)
+func (m *CreateShippingResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateShippingResponse.Unmarshal(m, b)
 }
-func (m *CreateOrderResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_CreateOrderResponse.Marshal(b, m, deterministic)
+func (m *CreateShippingResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateShippingResponse.Marshal(b, m, deterministic)
 }
-func (m *CreateOrderResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreateOrderResponse.Merge(m, src)
+func (m *CreateShippingResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateShippingResponse.Merge(m, src)
 }
-func (m *CreateOrderResponse) XXX_Size() int {
-	return xxx_messageInfo_CreateOrderResponse.Size(m)
+func (m *CreateShippingResponse) XXX_Size() int {
+	return xxx_messageInfo_CreateShippingResponse.Size(m)
 }
-func (m *CreateOrderResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_CreateOrderResponse.DiscardUnknown(m)
+func (m *CreateShippingResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateShippingResponse.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_CreateOrderResponse proto.InternalMessageInfo
-
-func (m *CreateOrderResponse) GetOrderId() int32 {
-	if m != nil {
-		return m.OrderId
-	}
-	return 0
-}
+var xxx_messageInfo_CreateShippingResponse proto.InternalMessageInfo
 
 func init() {
-	proto.RegisterType((*CreateOrderRequest)(nil), "CreateOrderRequest")
-	proto.RegisterType((*Item)(nil), "Item")
-	proto.RegisterType((*CreateOrderResponse)(nil), "CreateOrderResponse")
+	proto.RegisterType((*CreateShippingRequest)(nil), "CreateShippingRequest")
+	proto.RegisterType((*CreateShippingResponse)(nil), "CreateShippingResponse")
 }
 
 func init() {
@@ -164,22 +100,16 @@ func init() {
 }
 
 var fileDescriptor_218d76102d381bfb = []byte{
-	// 258 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0x3f, 0x4f, 0xc3, 0x30,
-	0x10, 0xc5, 0x69, 0xd3, 0xa4, 0x70, 0xdd, 0x5c, 0xa4, 0x86, 0x32, 0x10, 0x65, 0xca, 0xe4, 0xa0,
-	0x22, 0x06, 0x84, 0x84, 0x54, 0x18, 0xaa, 0x4c, 0x20, 0x8f, 0x2c, 0x55, 0x6a, 0x9f, 0x52, 0x8b,
-	0xfa, 0x0f, 0xb6, 0xc3, 0xe7, 0x47, 0x71, 0x55, 0x24, 0x04, 0xdb, 0xdd, 0xcf, 0xcf, 0x4f, 0xef,
-	0x1e, 0x2c, 0xfc, 0x5e, 0x5a, 0x2b, 0x75, 0x57, 0x9f, 0x06, 0x6a, 0x9d, 0x09, 0xa6, 0xfc, 0x00,
-	0xf2, 0xe2, 0xb0, 0x0d, 0xf8, 0xea, 0x04, 0x3a, 0x86, 0x9f, 0x3d, 0xfa, 0x40, 0x16, 0x30, 0xed,
-	0x3d, 0xba, 0xad, 0x14, 0xf9, 0xa8, 0x18, 0x55, 0x09, 0xcb, 0x86, 0xb5, 0x11, 0xe4, 0x1a, 0x52,
-	0x19, 0x50, 0xf9, 0x7c, 0x5c, 0x24, 0xd5, 0x6c, 0x95, 0xd2, 0x26, 0xa0, 0x62, 0x47, 0x46, 0x6e,
-	0x60, 0x16, 0x4c, 0x68, 0x0f, 0x5b, 0xeb, 0x24, 0xc7, 0x3c, 0x29, 0x46, 0xd5, 0x98, 0x41, 0x44,
-	0x6f, 0x03, 0x29, 0x97, 0x30, 0x19, 0xf4, 0x84, 0xc0, 0x44, 0xb7, 0x0a, 0xa3, 0xf7, 0x05, 0x8b,
-	0x73, 0x79, 0x0b, 0xf3, 0x5f, 0x41, 0xbc, 0x35, 0xda, 0x23, 0xb9, 0x82, 0x73, 0x33, 0x80, 0x53,
-	0x94, 0x94, 0x4d, 0xe3, 0xde, 0x88, 0xd5, 0x13, 0xa4, 0x51, 0x4b, 0xee, 0x21, 0x3b, 0x7e, 0x25,
-	0x73, 0xfa, 0xf7, 0x98, 0xe5, 0x25, 0xfd, 0xc7, 0xb8, 0x3c, 0x7b, 0x7e, 0x7c, 0x7f, 0xe8, 0x64,
-	0xd8, 0xf7, 0x3b, 0xca, 0x8d, 0xaa, 0x37, 0xe6, 0xd0, 0xea, 0x6e, 0xad, 0xc5, 0xc6, 0x59, 0x5e,
-	0x2b, 0xc9, 0x9d, 0xf1, 0xe8, 0xbe, 0x24, 0xc7, 0xb5, 0x95, 0xbe, 0xee, 0xe2, 0xf3, 0x4f, 0x7b,
-	0xbb, 0x2c, 0xd6, 0x77, 0xf7, 0x1d, 0x00, 0x00, 0xff, 0xff, 0xec, 0xb6, 0xb0, 0xbd, 0x59, 0x01,
-	0x00, 0x00,
+	// 176 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2f, 0xce, 0xc8, 0x2c,
+	0x28, 0xc8, 0xcc, 0x4b, 0xd7, 0x87, 0x31, 0xf4, 0x0a, 0x8a, 0xf2, 0x4b, 0xf2, 0x95, 0x0c, 0xb9,
+	0x44, 0x9d, 0x8b, 0x52, 0x13, 0x4b, 0x52, 0x83, 0xa1, 0xe2, 0x41, 0xa9, 0x85, 0xa5, 0xa9, 0xc5,
+	0x25, 0x42, 0x12, 0x5c, 0xec, 0x89, 0x29, 0x29, 0x45, 0xa9, 0xc5, 0xc5, 0x12, 0x8c, 0x0a, 0x8c,
+	0x1a, 0x9c, 0x41, 0x30, 0xae, 0x92, 0x04, 0x97, 0x18, 0xba, 0x96, 0xe2, 0x82, 0xfc, 0xbc, 0xe2,
+	0x54, 0x23, 0x77, 0x2e, 0x0e, 0x98, 0x98, 0x90, 0x35, 0x17, 0x1b, 0x44, 0x95, 0x90, 0x98, 0x1e,
+	0x56, 0x1b, 0xa4, 0xc4, 0xf5, 0xb0, 0x1b, 0xa3, 0xc4, 0xe0, 0x64, 0x1d, 0x65, 0x99, 0x9e, 0x59,
+	0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0xef, 0x9e, 0x9f, 0x93, 0x98, 0x97, 0xee, 0x98,
+	0x97, 0xe2, 0x5e, 0x54, 0x90, 0xac, 0x9f, 0x9b, 0x99, 0x5c, 0x94, 0x5f, 0x9c, 0x5a, 0x54, 0x96,
+	0x99, 0x9c, 0xea, 0x58, 0x90, 0x59, 0xac, 0x9f, 0x0e, 0x96, 0x86, 0x7b, 0x2c, 0x89, 0x0d, 0xec,
+	0x33, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x32, 0x92, 0x59, 0x02, 0xf4, 0x00, 0x00, 0x00,
 }
--- golang/shipping/shipping_grpc.pb.go
+++ golang/shipping/shipping_grpc.pb.go
@@ -19,90 +19,90 @@ import (
 const _ = grpc.SupportPackageIsVersion8
 
 const (
-	Order_Create_FullMethodName = "/Order/Create"
+	Shipping_Create_FullMethodName = "/Shipping/Create"
 )
 
-// OrderClient is the client API for Order service.
+// ShippingClient is the client API for Shipping service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type OrderClient interface {
-	Create(ctx context.Context, in *CreateOrderRequest, opts ...grpc.CallOption) (*CreateOrderResponse, error)
+type ShippingClient interface {
+	Create(ctx context.Context, in *CreateShippingRequest, opts ...grpc.CallOption) (*CreateShippingResponse, error)
 }
 
-type orderClient struct {
+type shippingClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewOrderClient(cc grpc.ClientConnInterface) OrderClient {
-	return &orderClient{cc}
+func NewShippingClient(cc grpc.ClientConnInterface) ShippingClient {
+	return &shippingClient{cc}
 }
 
-func (c *orderClient) Create(ctx context.Context, in *CreateOrderRequest, opts ...grpc.CallOption) (*CreateOrderResponse, error) {
+func (c *shippingClient) Create(ctx context.Context, in *CreateShippingRequest, opts ...grpc.CallOption) (*CreateShippingResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(CreateOrderResponse)
-	err := c.cc.Invoke(ctx, Order_Create_FullMethodName, in, out, cOpts...)
+	out := new(CreateShippingResponse)
+	err := c.cc.Invoke(ctx, Shipping_Create_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// OrderServer is the server API for Order service.
-// All implementations must embed UnimplementedOrderServer
+// ShippingServer is the server API for Shipping service.
+// All implementations must embed UnimplementedShippingServer
 // for forward compatibility
-type OrderServer interface {
-	Create(context.Context, *CreateOrderRequest) (*CreateOrderResponse, error)
-	mustEmbedUnimplementedOrderServer()
+type ShippingServer interface {
+	Create(context.Context, *CreateShippingRequest) (*CreateShippingResponse, error)
+	mustEmbedUnimplementedShippingServer()
 }
 
-// UnimplementedOrderServer must be embedded to have forward compatible implementations.
-type UnimplementedOrderServer struct {
+// UnimplementedShippingServer must be embedded to have forward compatible implementations.
+type UnimplementedShippingServer struct {
 }
 
-func (UnimplementedOrderServer) Create(context.Context, *CreateOrderRequest) (*CreateOrderResponse, error) {
+func (UnimplementedShippingServer) Create(context.Context, *CreateShippingRequest) (*CreateShippingResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
 }
-func (UnimplementedOrderServer) mustEmbedUnimplementedOrderServer() {}
+func (UnimplementedShippingServer) mustEmbedUnimplementedShippingServer() {}
 
-// UnsafeOrderServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to OrderServer will
+// UnsafeShippingServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to ShippingServer will
 // result in compilation errors.
-type UnsafeOrderServer interface {
-	mustEmbedUnimplementedOrderServer()
+type UnsafeShippingServer interface {
+	mustEmbedUnimplementedShippingServer()
 }
 
-func RegisterOrderServer(s grpc.ServiceRegistrar, srv OrderServer) {
-	s.RegisterService(&Order_ServiceDesc, srv)
+func RegisterShippingServer(s grpc.ServiceRegistrar, srv ShippingServer) {
+	s.RegisterService(&Shipping_ServiceDesc, srv)
 }
 
-func _Order_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(CreateOrderRequest)
+func _Shipping_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateShippingRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(OrderServer).Create(ctx, in)
+		return srv.(ShippingServer).Create(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Order_Create_FullMethodName,
+		FullMethod: Shipping_Create_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(OrderServer).Create(ctx, req.(*CreateOrderRequest))
+		return srv.(ShippingServer).Create(ctx, req.(*CreateShippingRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Order_ServiceDesc is the grpc.ServiceDesc for Order service.
+// Shipping_ServiceDesc is the grpc.ServiceDesc for Shipping service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Order_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "Order",
-	HandlerType: (*OrderServer)(nil),
+var Shipping_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "Shipping",
+	HandlerType: (*ShippingServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Create",
-			Handler:    _Order_Create_Handler,
+			Handler:    _Shipping_Create_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
